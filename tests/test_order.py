5681c2727ec63d46af606b7be54146299650c59a
Oleg <olafieros@gmail.com>
2026-08-21 16:58:27 +0200

    test for story 9

---
+++ tests/test_order.py
@@ -0,0 +1,32 @@
+from decimal import Decimal
+
+from app.core.security import create_access_token
+from tests.factories import ProductFactory, UserFactory
+
+URL = "/api/order/new_order"
+
+
+async def test_create_an_order(client, persist):
+    user = UserFactory(email="oleg@test.com")
+    mug = ProductFactory(price=Decimal("110"))
+    mug2 = ProductFactory(price=Decimal("5.50"))
+    await persist(user, mug, mug2)
+    token = create_access_token(user.email)
+
+    response = await client.post(
+        URL,
+        headers={"Authorization": f"Bearer {token}"},
+        json={
+            "items": [
+                {"product_id": mug.id, "quantity": 2},
+                {"product_id": mug2.id, "quantity": 3},
+            ],
+            "payment_method": "card",
+            "delivery_method": "courier",
+            "billing_information": "string",
+        },
+    )
+
+    assert response.status_code == 201
+    assert response.json()["status"] == "pending"
+    assert Decimal(response.json()["total_amount"]) == Decimal("236.50")
